e010c4cadfc08b90f1aaffbf8b4938bdaec3e932
Kuumamon <jonshannow@outlook.fr>
2016-07-05 14:17:52 +0200

    Add doxygen

--- sources/header/LGCell.h
+++ sources/header/LGCell.h
@@ -1,20 +1,46 @@
 #ifndef DEF_LGCELL
 #define DEF_LGCELL
 
+/*!
+* \file LGCell.h
+* \brief Cell definition
+* \author Alexis Koutero
+* \version 0.1
+*/
+
 #include <iostream>
 
+/*! \class LGCell
+* \brief Class representing a cell
+*/
 class LGCell {
 private:
-	bool m_isAlive;
+	bool m_isAlive; /*!< Cell's status*/
 
 public:
-	/*** CONSTRUCTORS ***/
+	/*!
+	*  \brief Default constructor
+	*/
 	LGCell();
+
+	/*!
+	*  \brief Constructor with parameters
+	*
+	*  \param IsAlive : if true, the cell is setted as alive
+	*/
 	LGCell(bool IsAlive);
 	LGCell(const LGCell& Cell);
 
-	/*** ACCESSORS ***/
+	/*!
+	*  \brief Is this cell alive?
+	*/
 	bool isAlive() const;
+
+	/*!
+	*  \brief Modify cell's status
+	*
+	*  \param IsAlive : if true, the cell is setted as alive
+	*/
 	void setIsAlive(bool IsAlive);
 };
 #endif
--- sources/header/LGGrid.h
+++ sources/header/LGGrid.h
@@ -1,49 +1,140 @@
 #ifndef DEF_LGGRID
 #define DEF_LGGRID
+
+/*!
+* \file LGGrid.h
+* \brief Grid definition
+* \author Alexis Koutero
+* \version 0.1
+*/
+
 #include "LGCell.h"
 
+/*! \class LGCell
+* \brief Class managing a grod of cells and it's behavior
+*/
 class LGGrid {
 private:
-	int m_xTechSize;
-	int m_yTechSize;
-	int m_xRealSize;
-	int m_yRealSize;
-	LGCell* m_cellGrid;
+	int m_xTechSize; /*!< Current number of line of the board including empty lines*/
+	int m_yTechSize; /*!< Current number of column of the board including empty lines*/
+	int m_xRealSize; /*!< Number of line of the board*/
+	int m_yRealSize; /*!< Number of column of the board*/
+	LGCell* m_cellGrid; /*!< The board*/
 
-	/*** For stats***/
-	int m_totalAliveCells;
-	int m_totalDeath;
-	int m_totalBirth;
-	int m_totalExecution;
+	int m_totalAliveCells; /*!< Number of curent alive cells*/
+	int m_totalDeath; /*!< Number of cell which passed away*/
+	int m_totalBirth; /*!< Number of cell which had borne*/
+	int m_totalExecution; /*!< Number of life cycle*/
 
-	/*** LIFE GAME ENGINE ***/
+	/*! \brief return a copy of the current grid
+	*
+	* \return a pointer to the new grid
+	*/
 	LGGrid* copyGrid();
+
+	/*! \brief Copy the grid into the current
+	*
+	* \param LGGrid : Crid to copy
+	*/
 	void updateGrid(LGGrid const * Grid);
 
 public:
-	/*** CONSTRUCTORS ***/
+	/*! \brief Defaul constructor
+	*
+	* Default size is 5 by 5 grid is all cells dead
+	*/
 	LGGrid();
+
+	/*! \brief Constructor to build a specific sized grid
+	*
+	* All cells are dead
+	*
+	* \param XSize : number of line without empty ones
+	* \param YSize : number of column without empty ones
+	*/
 	LGGrid(int XSize, int YSize);
+
+	/*! \brief Copy constructor
+	*
+	* \param Grid : creates a new grid based on the parameter
+	*/
 	LGGrid(const LGGrid&  Grid);
 
-	/*** DESTRUCTOR ***/
+	/*! \brief Destructor
+	*
+	* Desallocates grid and cells.
+	*/
 	~LGGrid();
 
-	/*** ACCESSOR ***/
+	/*! \brief Return number of line
+	*
+	* \return number of line without empty ones
+	*/
 	int getXSize() const;
+
+	/*! \brief Return number of column
+	*
+	* \return number of column without empty ones
+	*/
 	int getYSize() const;
+
+	/*! \brief Is a cell alive?
+	*
+	* Check if a cell is alive or not. Manage empty cells therefore parameters should be as for the real grid only
+	*
+	* \param X : line position
+	* \param Y : column position
+	* \return true if the cell is alive
+	*/
 	bool isCellAlive(int X, int Y) const;
+
+	/*! \brief How many cells are alive?
+	*
+	* \return : total number of alive cells on the current grid
+	*/
 	int getTotalAliveCells() const;
 
+	/*! \brief Change cell's status
+	*
+	* \param X : line position
+	* \param Y : column position
+	*/
 	void setCellAlive(int X, int Y, bool IsAlive);
 
-	/*** LIFE GAME ENGINE ***/
+	/*! \brief Execute on turn of the game
+	*/
 	void execute();
 
-	/*** OTHERS ***/
+	/*! \brief Dispays on stdout all cells
+	*/
 	void display() const;
+
+	/*! \brief display on stdout all cells including empty edges
+	*/
 	void displayTech() const;
+
+	/*! \brief display current grid's stats
+	*
+	* Current stats are :
+	*	- Grid size
+	*	- Total cells
+	*	- Total alive cells
+	*	- Number of execution
+	*	- Number of death
+	*	- Number of birth
+	*/
 	void displayStats() const;
+
+	/*! \brief update statistics.
+	*
+	* Current stats are :
+	*	- Grid size
+	*	- Total cells
+	*	- Total alive cells
+	*	- Number of execution
+	*	- Number of death
+	*	- Number of birth
+	*/
 	void updateStats();
 };
 
