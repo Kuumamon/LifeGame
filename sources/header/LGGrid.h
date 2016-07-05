f915b7e4d24b2938e9b6bb171070933d14238a4a
Kuumamon <jonshannow@outlook.fr>
2016-07-05 09:05:16 +0200

    Add files via upload

--- sources/header/LGGrid.h
+++ sources/header/LGGrid.h
@@ -4,9 +4,21 @@
 
 class LGGrid {
 private:
-	int m_xSize;
-	int m_ySize;
+	int m_xTechSize;
+	int m_yTechSize;
+	int m_xRealSize;
+	int m_yRealSize;
 	LGCell* m_cellGrid;
+
+	/*** For stats***/
+	int m_totalAliveCells;
+	int m_totalDeath;
+	int m_totalBirth;
+	int m_totalExecution;
+
+	/*** LIFE GAME ENGINE ***/
+	LGGrid* copyGrid();
+	void updateGrid(LGGrid const * Grid);
 
 public:
 	/*** CONSTRUCTORS ***/
@@ -21,8 +33,18 @@ public:
 	int getXSize() const;
 	int getYSize() const;
 	bool isCellAlive(int X, int Y) const;
+	int getTotalAliveCells() const;
 
 	void setCellAlive(int X, int Y, bool IsAlive);
+
+	/*** LIFE GAME ENGINE ***/
+	void execute();
+
+	/*** OTHERS ***/
+	void display() const;
+	void displayTech() const;
+	void displayStats() const;
+	void updateStats();
 };
 
 #endif
